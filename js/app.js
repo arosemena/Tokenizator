19d7d75ee9af978f2edb3d05c7b224f589596bac
Roberto Arosemena <arosemenae@gmail.com>
2014-01-13 16:18:18 -0700

    Fixed a typo and an if statement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //  Tokenizator by Roberto Arosemena
-//  v 1.0.0
+//  v 1.0.1
 
 document.oncontextmenu = function() { return false; }
 
--- js/launch.js
+++ js/launch.js
@@ -2,10 +2,10 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.storage.local.get('bounds', function(o){
     var bounds = {width: 300, height: 400, top: 20, left: 20};
     if(typeof o.bounds !== 'undefined') {
-      if(typeof o.bounds.width === 'number' &&
-         typeof o.bounds.width === 'number' &&
-         typeof o.bounds.width === 'number' &&
-         typeof o.bounds.width === 'number') {
+      if(typeof o.bounds.width  === 'number' &&
+         typeof o.bounds.height === 'number' &&
+         typeof o.bounds.top    === 'number' &&
+         typeof o.bounds.left   === 'number') {
         // assign values directly to avoid injection
         bounds = {width  : o.bounds.width, 
                   height : o.bounds.height, 
